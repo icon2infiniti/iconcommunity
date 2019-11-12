3e552c0dea7725046c2083bd4ae5f0e3d59f3bf1
2infiniti <30359168+2infiniti@users.noreply.github.com>
2019-11-12 20:44:31 +0800

    turn off debug

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path('set_wallet/<str:fromAddress>/', views.set_wallet, name='set_wallet'),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
