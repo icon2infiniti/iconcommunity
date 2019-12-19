103732cd3b493d3107d36669def473bc3d915cf9
2infiniti <30359168+2infiniti@users.noreply.github.com>
2019-12-19 09:00:07 +0800

    news section

--- news/views.py
+++ news/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from .models import News, Press, Video
-from el_pagination.decorators import page_template
 
-import feedparser
-from bs4 import BeautifulSoup
+#import feedparser
+#from bs4 import BeautifulSoup
 
-from datetime import datetime
+#from datetime import datetime
 from dateutil.parser import parse
 import tweepy
+import praw
 
 
 def init_mode(request):
@@ -22,59 +21,15 @@ def init_mode(request):
         'nightmode': request.session['nightmode'],
         'navbar': request.session['navbar'],
         'fromAddress': request.session['fromAddress'],
-        'section': 'RESOURCES',
+        'section': 'NEWS',
     }
     return context
 
 
-@page_template('resources/video_general_page.html', key='video_general_page')
-@page_template('resources/video_events_page.html', key='video_events_page')
-@page_template('resources/video_interviews_page.html', key='video_interviews_page')
-def collateral(request, template='resources/collateral.html', extra_context=None):
+def news(request, template='news/news.html', extra_context=None):
     context = init_mode(request)
 
-    video_generals = Video.objects.filter(video_category="General").order_by('-video_date')
-    for url in video_generals:
-        url.video_link = url.video_link.replace("watch?v=", "embed/")+"?rel=0"
-
-    video_events = Video.objects.filter(video_category="Events").order_by('-video_date')
-    for url in video_events:
-        url.video_link = url.video_link.replace("watch?v=", "embed/")+"?rel=0"
-
-    video_interviews = Video.objects.filter(video_category="Interviews").order_by('-video_date')
-    for url in video_interviews:
-        url.video_link = url.video_link.replace("watch?v=", "embed/")+"?rel=0"
-
-    context.update({
-        'video_generals': video_generals,
-        'video_events': video_events,
-        'video_interviews': video_interviews,
-        'subsection': 'COLLATERAL',
-    })
-
-    if extra_context is not None:
-        context.update(extra_context)
-    return render(request, template, context)
-
-
-@page_template('resources/news_page.html', key='news_page')
-@page_template('resources/press_page.html', key='press_page')
-def press(request, template='resources/press.html', extra_context=None):
-    context = init_mode(request)
-    context.update({
-        'news': News.objects.all().order_by('-news_date'),
-        'presses': Press.objects.all().order_by('-press_date'),
-        'subsection': 'PRESS',
-    })
-
-    if extra_context is not None:
-        context.update(extra_context)
-    return render(request, template, context)
-
-
-def news_old(request, template='resources/news.html', extra_context=None):
-    context = init_mode(request)
-
+    '''
     REDDIT = feedparser.parse('https://reddit.com/r/helloicon.rss')
     reddit_entries = REDDIT['entries'][0:10]
     for entry in reddit_entries:
@@ -84,6 +39,7 @@ def news_old(request, template='resources/news.html', extra_context=None):
             entry['thumb'] = imgtag['src']
         entry['author'] = entry['author'][3:len(entry['author'])]
         entry['updated'] = parse(entry['updated'])
+    '''
 
     '''
     MEDIUM = feedparser.parse('https://medium.com/feed/helloiconworld')
@@ -103,13 +59,30 @@ def news_old(request, template='resources/news.html', extra_context=None):
             entry['thumb'] = imgtag['src']
     '''
 
+    #Twitter
     MAX_TWEETS = 10
     auth = tweepy.OAuthHandler("sq3iEj5FrRHtuZdHG209GNhNX", "it8cSeHYGPPB6pegyzgKUr9rZ4pT05NJVnQM0d3g5cpxTYdffx")
     api = tweepy.API(auth)
-    twitter_entries = [status._json for status in tweepy.Cursor(api.search,  q='$ICX OR #ICX OR #ICONProject -filter:retweets').items(MAX_TWEETS)]
+    twitter_entries = [status._json for status in tweepy.Cursor(api.search,  q='#ICONProject -filter:retweets').items(MAX_TWEETS)]
 
     for dt in twitter_entries:
         dt['created_at'] = parse(dt['created_at'])
+
+    # Reddit
+    reddit = praw.Reddit(client_id='GYqmrmi5cunm1A',
+                         client_secret='rydmYFNqBKpnwkvjwybmfNzSY-g',
+                         user_agent='icon.community')
+
+    reddit_entries = reddit.subreddit('helloicon').hot(limit=10)
+
+    '''
+    for submission in submissions:
+        print(submission.title)  # Output: the submission's title
+        print(submission.score)  # Output: the submission's score
+        print(submission.id)  # Output: the submission's ID
+        print(submission.url)  # Output: the URL the submission points to
+        print(submission.num_comments)
+    '''
 
     context.update({
         'subsection': 'NEWS',
